a19a28384a1e0e525b627faac11ef970aeb3634c
P_M <Student@LabVM2049939>
2026-02-28 17:43:50 +0200

    Question1 of Part1 - Voice Greeting

--- Program.cs
+++ Program.cs
@@ -3,13 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Media;
 
 namespace Cybersecurity_Awareness_ChatBot
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main(string[] args) 
         {
+            //Create sound greeting message to welcome users to the chatbot
+            PlaySound();
+
         }
+
+        public static void PlaySound() { 
+        
+            var soundPlayer = new System.Media.SoundPlayer();
+            soundPlayer.SoundLocation = @"C:\Users\Student\Desktop\Recording.wav";
+            soundPlayer.PlaySync();
+        }
+
+        
     }
 }
