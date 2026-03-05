6e7a503fa8a6aac5094fd4876f852df1fcd7cc52
P_M <Student@LabVM2049939>
2026-03-05 20:07:09 +0200

    Question 2 & 3 of Part1 - Image display & Text-based greeting and User interaction

--- Program.cs
+++ Program.cs
@@ -1,28 +1,79 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Media;
 
 namespace Cybersecurity_Awareness_ChatBot
 {
     internal class Program
     {
-        static void Main(string[] args) 
+        static void Main(string[] args)
         {
+
             //Create sound greeting message to welcome users to the chatbot
             PlaySound();
 
+            //Display chatbot logo
+            DisplayLogo();
+
+            //Greet user and display welcome message
+            GreetUser();
         }
 
-        public static void PlaySound() { 
-        
+        public static void PlaySound()
+        {
+            //Create a SoundPlayer instance and set the sound location to the desired audio file
             var soundPlayer = new System.Media.SoundPlayer();
             soundPlayer.SoundLocation = @"C:\Users\Student\Desktop\Recording.wav";
             soundPlayer.PlaySync();
         }
 
-        
+        public static void DisplayLogo()
+        {
+            //Create a simple ASCII art logo for the chatbot
+            Console.WriteLine(@"
+              |`-._/\_.-`|
+              |    C-S   |
+              |___o()o___|
+              |__((<>))__|
+              \   o\/o   /
+               \   A-B  /
+                \  ||  /
+                 '.||.'
+                   ``
+
+
+");
+        }
+
+        public static void GreetUser()
+        {
+            //Display a welcome banner to the user
+            Console.WriteLine(
+                @"╔~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^-╗
+│                                                                     │
+│        Welcome to the Cybersecurity Awareness ChatBot!              │
+│                                  *                                  │
+│                                 CSAB                                │                    
+╚~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~╝");
+
+            Console.WriteLine();
+
+            Console.WriteLine("Please enter your name: ");//Prompt the user to enter their name
+
+            Console.WriteLine();
+
+            string name = Console.ReadLine();//Read the user's input and store it in a variable
+            Console.WriteLine();
+
+            Console.WriteLine($"Hello {name}!");//Greet the user by name
+
+
+        }
+
+
+
+
     }
+
+   
+
+
 }
